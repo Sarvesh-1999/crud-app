57d8d582e8c488991e315f494a920530664be450
Sarvesh <sarvesh.kumar.jsr@gmail.com>
2025-11-18 13:32:44 +0530

    login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
 import { myRoutes } from './routes/Routing'
+import { Toaster } from 'react-hot-toast'
 const App = () => {
   return (
     <div>
       <RouterProvider router={myRoutes}/>
+      <Toaster/>
     </div>
   )
 }
